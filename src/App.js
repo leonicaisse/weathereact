a32cbb0ba4ec8eb7ad29a81e0e94f5414e465b53
Clément <clmntrss.pro@gmail.com>
2018-06-04 22:45:39 +0900

    Ajout de la date

--- src/App.js
+++ src/App.js
@@ -3,15 +3,37 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    
+    constructor() {
+        super();
+        let CurrentDay = new Date(),
+            day = CurrentDay.getDay();
+        let CurrentMonth = new Date(),
+            month = CurrentMonth.getMonth();
+        let NumberDay = new Date(),
+            number = NumberDay.getDate()
+        this.state = {
+            day: day,
+            month: month,
+            number: number,
+        };
+
+    }  
     render() {
+        const monthNames = ["Janvier", "Fevrier", "Mars", "Avril", "Mai","Juin","Juillet", "Aout", "Septembre", "Octobre", "Novembre","Decembre"];
+        const dayNames = ["Dimanche","Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi"];
+        let day=this.state.day;
+        let month=this.state.month;
+        let number= this.state.number;
         return (
+            
             <div className="App">
                 <div className="currentContainer">
                     <p className="city"><span className="cityName">Paris</span>, France</p>
                     <div className="currentWeather">
                         <div className="blockDate">
                             <p className="currentTime">10:00</p>
-                            <p className="currentDay">lun. 4 juin</p>
+                            <p className="currentDay">{dayNames[day]} {number} {monthNames[month]}</p>
                         </div>
                         <div className="blockWeather">
                             {/*::before soleil*/}
