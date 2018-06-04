58a255e574a3c4274ce1f9c162232414b3e9fdbf
Léo Nicaisse <leonicaisse@gmail.com>
2018-06-04 14:41:57 +0200

    Premiere intégration du style du bloc "météo en temps réel"

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div className="currentContainer">
+                    <p className="city"><span className="cityName">Paris</span>, France</p>
+                    <div className="currentWeather">
+                        <div className="blockDate">
+                            <p className="currentTime">10:00</p>
+                            <p className="currentDay">lun. 4 juin</p>
+                        </div>
+                        <div className="blockWeather">
+                            {/*::before soleil*/}
+                            <p className="currentTemperature">19°c</p>
+                            <div className="currentInfos">
+                                <p className="currentSky">Ensoleillé</p>
+                                {/*::before goute*/}
+                                <p className="currentRain">5%</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="dayWeather"></div>
+                <div className="weekWeather"></div>
+            </div>
+        );
+    }
 }
 
 export default App;
